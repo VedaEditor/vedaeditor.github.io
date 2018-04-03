ef9daf833de49b0b27fcdf3e912da2ffdcc14587
sergBull <bullsergv@gmail.com>
2018-04-03 17:04:49 +0300

    v0.05

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,3 @@ var render = document.querySelector(".Renderer");
 render.style.background = "rgb(127, 127, 127)";
 render.style.height = "100vh";
 render.style.width = window.innerWidth + "px";
-
-var veda = new Veda();
-
-veda.GUI.Button("body", {
-	click: function() {
-		alert(1);
-	},
-	text: "Hello, World!",
-	styles: "position: absolute;"
-});
